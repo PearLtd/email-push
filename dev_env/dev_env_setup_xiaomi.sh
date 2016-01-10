96d74a257118a3793ec999d2fb29cd53f2084914
Cheedoong <cheedoong@acm.org>
2016-01-10 14:18:02 +0800

    tested dev env setup

---
+++ dev_env/dev_env_setup_xiaomi.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+wget http://bigota.miwifi.com/xiaoqiang/sdk/tools/package/sdk_package.zip -O sdk_package.zip
+unzip sdk_package.zip -d ./
+
+export CHROOT=$(pwd)/sdk_package
+export TOOLCHAIN_PATH=$CHROOT/toolchain
+export PATH=$PATH:$TOOLCHAIN_PATH/usr/lib
+export AR=$TOOLCHAIN_PATH/bin/arm-xiaomi-linux-uclibcgnueabi-ar
+export AS=$TOOLCHAIN_PATH/bin/arm-xiaomi-linux-uclibcgnueabi-as
+export LD=$TOOLCHAIN_PATH/bin/arm-xiaomi-linux-uclibcgnueabi-ld
+export NM=$TOOLCHAIN_PATH/bin/arm-xiaomi-linux-uclibcgnueabi-nm
+export CC=$TOOLCHAIN_PATH/bin/arm-xiaomi-linux-uclibcgnueabi-gcc
+export CPP=$TOOLCHAIN_PATH/bin/arm-xiaomi-linux-uclibcgnueabi-cpp
+export GCC=$TOOLCHAIN_PATH/bin/arm-xiaomi-linux-uclibcgnueabi-gcc
+export CXX=$TOOLCHAIN_PATH/bin/arm-xiaomi-linux-uclibcgnueabi-g++
+export RANLIB=$TOOLCHAIN_PATH/bin/arm-xiaomi-linux-uclibcgnueabi-ranlib
+
+TOOLCHAIN_PROFILE=/etc/profile.d/xiaomi_router.sh
+sudo rm -rf $TOOLCHAIN_PROFILE
+sudo touch $TOOLCHAIN_PROFILE
+sudo echo "#!/bin/sh" >> $TOOLCHAIN_PROFILE
+sudo echo "export CHROOT=./sdk_package" >> $TOOLCHAIN_PROFILE
+sudo echo "export TOOLCHAIN_PATH=$CHROOT/toolchain" >> $TOOLCHAIN_PROFILE
+sudo echo "export PATH=$PATH:$TOOLCHAIN_PATH/usr/lib" >> $TOOLCHAIN_PROFILE
+sudo echo "export AR=$TOOLCHAIN_PATH/bin/arm-xiaomi-linux-uclibcgnueabi-ar" >> $TOOLCHAIN_PROFILE
+sudo echo "export AS=$TOOLCHAIN_PATH/bin/arm-xiaomi-linux-uclibcgnueabi-as" >> $TOOLCHAIN_PROFILE
+sudo echo "export LD=$TOOLCHAIN_PATH/bin/arm-xiaomi-linux-uclibcgnueabi-ld" >> $TOOLCHAIN_PROFILE
+sudo echo "export NM=$TOOLCHAIN_PATH/bin/arm-xiaomi-linux-uclibcgnueabi-nm" >> $TOOLCHAIN_PROFILE
+sudo echo "export CC=$TOOLCHAIN_PATH/bin/arm-xiaomi-linux-uclibcgnueabi-gcc" >> $TOOLCHAIN_PROFILE
+sudo echo "export CPP=$TOOLCHAIN_PATH/bin/arm-xiaomi-linux-uclibcgnueabi-cpp" >> $TOOLCHAIN_PROFILE
+sudo echo "export GCC=$TOOLCHAIN_PATH/bin/arm-xiaomi-linux-uclibcgnueabi-gcc" >> $TOOLCHAIN_PROFILE
+sudo echo "export CXX=$TOOLCHAIN_PATH/bin/arm-xiaomi-linux-uclibcgnueabi-g++" >> $TOOLCHAIN_PROFILE
+sudo echo "export RANLIB=$TOOLCHAIN_PATH/bin/arm-xiaomi-linux-uclibcgnueabi-ranlib" >> $TOOLCHAIN_PROFILE
+
+sudo apt-get autoremove
+sudo apt-get install build-essential
+sudo apt-get install automake
+sudo apt-get install autoconf
+sudo apt-get install libtool
+sudo apt-get install pkg-config
+sudo apt-get install intltool 
+
+
+
