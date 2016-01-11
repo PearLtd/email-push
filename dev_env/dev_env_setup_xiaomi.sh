071ec97ae8917455cd68a568a1e7830d33838fa2
Cheedoong Ch'ng <cheedoong@acm.org>
2016-01-11 23:28:36 +0800

    Update dev_env_setup_xiaomi.sh

--- dev_env/dev_env_setup_xiaomi.sh
+++ dev_env/dev_env_setup_xiaomi.sh
@@ -5,7 +5,11 @@ unzip sdk_package.zip -d ./
 
 export CHROOT=$(pwd)/sdk_package
 export TOOLCHAIN_PATH=$CHROOT/toolchain
-export PATH=$PATH:$TOOLCHAIN_PATH/usr/lib
+export PATH=$TOOLCHAIN_PATH/bin:$TOOLCHAIN_PATH/arm-xiaomi-linux-uclibcgnueabi/bin:$PATH
+export CPATH=$CHROOT/include:$CPATH
+export C_INCLUDE_PATH=$CHROOT/include:$TOOLCHAIN_PATH/arm-xiaomi-linux-uclibcgnueabi/sysroot/usr/include:$C_INCLUDE_PATH
+export CPLUS_INCLUDE_PATH=$CHROOT/include:$TOOLCHAIN_PATH/arm-xiaomi-linux-uclibcgnueabi/include/c++/4.8.2/arm-xiaomi-linux-uclibcgnueabi:$CPLUS_INCLUDE_PATH
+export LD_LIBRARY_PATH=$CHROOT/lib:$TOOLCHAIN_PATH/usr/lib:$LD_LIBRARY_PATH
 export AR=$TOOLCHAIN_PATH/bin/arm-xiaomi-linux-uclibcgnueabi-ar
 export AS=$TOOLCHAIN_PATH/bin/arm-xiaomi-linux-uclibcgnueabi-as
 export LD=$TOOLCHAIN_PATH/bin/arm-xiaomi-linux-uclibcgnueabi-ld
@@ -20,9 +24,13 @@ TOOLCHAIN_PROFILE=/etc/profile.d/xiaomi_router.sh
 sudo rm -rf $TOOLCHAIN_PROFILE
 sudo touch $TOOLCHAIN_PROFILE
 sudo echo "#!/bin/sh" >> $TOOLCHAIN_PROFILE
-sudo echo "export CHROOT=./sdk_package" >> $TOOLCHAIN_PROFILE
+sudo echo "export CHROOT=$(pwd)/sdk_package" >> $TOOLCHAIN_PROFILE
 sudo echo "export TOOLCHAIN_PATH=$CHROOT/toolchain" >> $TOOLCHAIN_PROFILE
-sudo echo "export PATH=$PATH:$TOOLCHAIN_PATH/usr/lib" >> $TOOLCHAIN_PROFILE
+sudo echo "export PATH=$TOOLCHAIN_PATH/bin:$TOOLCHAIN_PATH/arm-xiaomi-linux-uclibcgnueabi/bin:$PATH" >> $TOOLCHAIN_PROFILE
+sudo echo "export CPATH=$CHROOT/include:$CPATH" >> $TOOLCHAIN_PROFILE
+sudo echo "export C_INCLUDE_PATH=$CHROOT/include:$TOOLCHAIN_PATH/arm-xiaomi-linux-uclibcgnueabi/sysroot/usr/include:$C_INCLUDE_PATH" >> $TOOLCHAIN_PROFILE
+sudo echo "export CPLUS_INCLUDE_PATH=$CHROOT/include:$TOOLCHAIN_PATH/arm-xiaomi-linux-uclibcgnueabi/include/c++/4.8.2/arm-xiaomi-linux-uclibcgnueabi:$CPLUS_INCLUDE_PATH" >> $TOOLCHAIN_PROFILE
+sudo echo "export LD_LIBRARY_PATH=$CHROOT/lib:$TOOLCHAIN_PATH/usr/lib:$LD_LIBRARY_PATH" >> $TOOLCHAIN_PROFILE
 sudo echo "export AR=$TOOLCHAIN_PATH/bin/arm-xiaomi-linux-uclibcgnueabi-ar" >> $TOOLCHAIN_PROFILE
 sudo echo "export AS=$TOOLCHAIN_PATH/bin/arm-xiaomi-linux-uclibcgnueabi-as" >> $TOOLCHAIN_PROFILE
 sudo echo "export LD=$TOOLCHAIN_PATH/bin/arm-xiaomi-linux-uclibcgnueabi-ld" >> $TOOLCHAIN_PROFILE
